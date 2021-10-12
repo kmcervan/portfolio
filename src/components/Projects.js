3d8dea905e164c8be7f9752dfb942ced6d29a8ef
Karla Cervantes <cervantes.karla14@gmail.com>
2021-10-12 12:59:12 -0500

    added max width, fixed navbar bugs, fixed project photo placements

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,22 +7,36 @@ import Ten from '../pictures/10.png';
 function Projects() {
     return (
         <section id='projects-page' className='projects-main'>
-            <h3 className='project-h3'>Check out past projects</h3>
+            <h3 className='project-h3'>Projects</h3>
             <div className='project-container'>
                 <div className='projects-container'>
-                    <a href='https://spacestagram200.herokuapp.com/' target='_blank'>
-                    <img src={Space} className='project-img'/>
+                    <div className='img-box'>
+                        <a href='https://spacestagram200.herokuapp.com/' target='_blank'>
+                            <img src={Space} className='project-img'/>
+                        </a>
+                    </div>
+                    <div className='project-text-box'>
+                        <h4>Spacestagram</h4>
+                        <p>The Spacestagram allows the user to see 30 randomly selected images from NASA's Astronomy Picture of the Day API. A title, photo, date and description are provided with every card. Users are able to like and unlike images by either clicking on the image or the like button.</p>
+                    </div>
+                </div>
+                <div className='projects-container'>
+                    <a target='_blank'>
+                        <img src={Nine} className='project-img'/>
                     </a>
-                    <div>
+                    <div className='project-text-box'>
                         <h4>Spacestagram</h4>
                         <p>Project description will go here</p>
                     </div>
                 </div>
                 <div className='projects-container'>
-                    <img src={Nine} className='project-img'/>
-                </div>
-                <div className='projects-container'>
-                    <img src={Ten} className='project-img'/>
+                    <a target='_blank'>
+                        <img src={Ten} className='project-img'/>
+                    </a>
+                    <div className='project-text-box'>
+                        <h4>Spacestagram</h4>
+                        <p>Project description will go here</p>
+                    </div>
                 </div>
             </div>
         </section>
