00c846d02d8119988f12d30a5152b937b0bdfab8
Karla Cervantes <cervantes.karla14@gmail.com>
2021-10-20 16:58:34 -0500

    added target page to icons

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -17,13 +17,13 @@ function MainPage() {
                         <h1 className='main-h1'>Karla Cervantes</h1>
                         <p className='main-p'>Hey, I'm Karla Cervantes &#9996;&#127997;! I am a full-stack developer ready to continue developing! While I do have a preference for a front-end development position, I am open to all possibilities. I've worked with PERN stack<span>PostgreSQL</span>, <span>Express</span>, <span>React</span>, and <span>Node</span>, but I am also very familiar with <span>Python</span>, <span>HTML</span>, and <span>CSS</span>.<br/><br/>A bit about myself I enjoy a good game of chess, long hikes, and reading/hearing new quotes. After watching an episode of Ted Lasso, my brain got suck on "Be curious, not judgemental" -Walt Whitman.<br/><br/>If I've managed to pique your interest please feel free to reach out!</p>
                     <div className='social-icons'>
-                        <a href='https://www.linkedin.com/in/kmc4/' className='contact-link'>
+                        <a href='https://www.linkedin.com/in/kmc4/' target='_blank' className='contact-link'>
                             <img className='link-img' src={Linkedin}/>
-                        </a><a href='https://github.com/kmcervan' className='contact-link'>
+                        </a><a href='https://github.com/kmcervan' target='_blank' className='contact-link'>
                             <img className='link-img' src={Git}/>
-                        </a><a href='https://twitter.com/Karla_mc4' className='contact-link'>
+                        </a><a href='https://twitter.com/Karla_mc4' target='_blank' className='contact-link'>
                             <img className='link-img' src={Eight}/>
-                        </a><a href='mailto:cervantes.karla14@gmail.com' className='contact-link'>
+                        </a><a href='mailto:cervantes.karla14@gmail.com' target='_blank' className='contact-link'>
                             <img className='link-img' src={Email}/>
                         </a>
                     </div>
